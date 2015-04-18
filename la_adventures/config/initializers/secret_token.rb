0aca435d3ca1293da3e40072ccfe31ae36bec44e
Aaron Harpole <aharpole@gmail.com>
2015-04-18 11:33:58 -0700

    initial rails app commit

---
+++ la_adventures/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LaAdventures::Application.config.secret_token = 'ae8a334faad5567bc915424b21fca3a675f138c81724c91793deadbd2e2f621475f2051ccbc2bfa884fc48d8ade1e0e12b8d46b5f323425c9d4c53a9c8c7dc60'
